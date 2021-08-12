986e12898a8dfbe438a37a1410bfeee3cb120f10
poly singh <poly.singh11@gmail.com>
2021-08-11 21:07:55 -0700

    updating server.js to fix await server error

--- server/server.js
+++ server/server.js
@@ -1,41 +1,69 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-
+const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
+
+// const routes = require('./routes');
+
+
+//import our typeDefs and resolvers
+// const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-const server = new ApolloServer({
+
+//create new Apollo server and pass in our schema data
+const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
-  context:authMiddleware
+  context: authMiddleware
 });
+apolloServer.applyMiddleware({ app });
+app.use(express.urlencoded({extended: true}));
+app.use(express.json)
+// async function startServer() {
+//     apolloServer = new ApolloServer({
+//         typeDefs,
+//         resolvers,
+//     });
+//     await apolloServer.start();
+//     apolloServer.applyMiddleware({ app });
+//   }
+// startServer();
 
-server.applyMiddleware({ app });
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, '../client/build')));
+    }
+    
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+    
+    
+    
+    // app.use(routes); 
+    
+    
+    db.once('open', () => {
+      app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        // log where we can go to test our GQL API
+        console.log(`Use GraphQL at http://localhost:${PORT}${apolloServer.graphqlPath}`);
+    
+      });
+    });
+    
+    process.on('uncaughtException', function(err) {
+      console.log('Caught exception: ' + err);
+    });
+  
+//integrate our Apollo server with Express application as middleware
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+
+
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () =>{
-    console.log(`🌍 Now listening on localhost:${PORT}`));
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
-
-process.on('uncaughtException', function(err) {
-  console.log('Caught exception: ' + err);
-});
